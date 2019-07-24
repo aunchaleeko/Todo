8e7f4c9b8fc43aaa5dc42dc74143ea76f9a23847
aunchalee <aunchalee.ko@hotmail.com>
2019-07-24 09:49:38 +0700

    i

---
+++ src/main/scala/com/rest/actors/MyTask.scala
@@ -0,0 +1,16 @@
+package com.rest.actors
+
+import akka.actor.{Actor, PoisonPill}
+import com.rest.messages.MyTodoMsg
+
+class MyTask(subject: String, detail: String, status: String) extends Actor {
+  import com.rest.messages.MyTaskMsg._
+
+  def receive: PartialFunction[Any, Unit] = {
+
+    case GetTask ⇒ sender() ! Some(MyTodoMsg.Task(subject, detail,status))
+    case Delete ⇒ sender() ! Some(MyTodoMsg.Task(subject, detail,status))
+      self ! PoisonPill
+  }
+}
+//helllo
